f944572a502e94ecf78ee589a00b7957900883e6
mbaddar1 <mbaddar2@gmail.com>
2016-01-03 15:37:50 +0200

    Added method to create xts instance in R workspace '
    This methods create timeDate index and xts instance in the given
    workspace.Method is added and unit test passed

--- src/timeseries/RTimeSeries.java
+++ src/timeseries/RTimeSeries.java
@@ -2,6 +2,15 @@ package timeseries;
 
 import java.time.ZonedDateTime;
 import java.util.Date;
+
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPLogical;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
+
+import datetime.converters.DateTimeConverters;
 /**
  * 
  * @author baddar
@@ -14,5 +23,58 @@ public class RTimeSeries extends TimeSeries {
 			double[] data,ZonedDateTime[] index) {
 		super(name, seasonality, data,index);
 	}
+	/**
+	 * Create xts time series in the given workspace with the given name based on the current time series
+	 * data and index
+	 * @param rconn R connection to workspace in which we create the variable
+	 * @param withVerfication call verification method to verify variable assigned in workspace is correct 
+	 * @param xtsInstanceName
+	 * @return REXP return from the {@code RConnection.eval used to create the xts time series}
+	 * @throws REngineException 
+	 * @throws REXPMismatchException 
+	 */
+	public REXP createRxtsInWorkSpace(RConnection rconn,String xtsInstanceName,boolean withVerfication) throws REngineException, REXPMismatchException {
+		//create data array in workspace
+		rconn.assign("data", this.getData());
+		rconn.assign("timeDateIndexChrVec", DateTimeConverters
+				.convertZonedDateTimeToRtimeDateStr(this.getIndex()));
+		//assume all ZonedDateTime instances in index have the same time zone
+		rconn.assign("timeZone",this.getIndex()[0].getZone().toString());
+		System.out.println(rconn.eval("timeZone").asString());
+		REXP ret = rconn.eval("require(timeDate)");
+		if(!ret.isLogical())
+			throw new RserveException(rconn, "return of expression require(timeDate) is not logical");
+		if(( (REXPLogical)ret).isFALSE()[0])
+			throw new RserveException(rconn, "Can't load timeDate library (may be not installed)");
+		String expr = "try(expr = {timeDateIndex <- timeDate(charvec = timeDateIndexChrVec"
+				+ ",format = \"%Y-%m-%d %H:%M:%S\",zone = timeZone)})";
+		
+		ret = rconn.eval(expr);
+		if(ret.isString())
+			throw new RserveException(rconn, "Error in evaluating experession to set "
+					+ "timeDateIndex : "+ret.asString());
+		else {
+			System.out.println("R timeDateIndex set successfully");
+		}
+		//create xts instance
+		//load xts library
+		ret = rconn.eval("require(xts)");
+		if(!ret.isLogical())
+			throw new RserveException(rconn, "return of expression require(xts) is not logical");
+		if( ((REXPLogical)ret).isFALSE()[0])
+			throw new RserveException(rconn, "Can't load xts library (may be not installed)");
+		
+		expr = "try(expr = {"+xtsInstanceName+" = xts(x = data,order.by = timeDateIndex,tzone = "
+				+ "timeZone,frequency = "+this.getFrequency()+")})";
+		ret = rconn.eval(expr);
+		if(ret.isString())
+			throw new RserveException(rconn, "Error in evaluating experession to set "
+					+ xtsInstanceName+" : "+ret.asString());
+		else {
+			System.out.println("R xts: "+xtsInstanceName+" set successfully");
+		}
+		return ret;
+	}
+	
 	
 }
